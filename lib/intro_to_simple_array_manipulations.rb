68fab153e15a94569b838797996c03849a03bd75
Robert Tate <btate712@gmail.com>
2019-02-26 16:40:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, number)
+  return_array = []
+  for item in 1..number 
+    return_array.push(array.pop)
+  end
+  return_array
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, number)
+  return_array = []
+  for item in 1..number
+    return_array.push(array.shift)
+  end
+  return_array
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(3, element)
+end
